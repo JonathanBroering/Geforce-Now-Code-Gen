78228fa96b5a11ae98602dbaddb7c08550dd0749
jnthn-b <JayJay150902@gmail.com>
2018-06-03 14:54:36 +0200

    Add files via upload

---
+++ geforce now.py
@@ -0,0 +1,33 @@
+import random
+print('This is a free Project by Jonathan (Jnthn) licenced under the MIT License. [jnthn.party]')
+code = random.randint(1,4)
+if code == 1:
+	randomint = random.randint(1,999999)
+	if randomint <= 9:
+		randomint = str(randomint)
+		randomint = '0' + randomint
+	print(str(randomint) + '-abdnwj-bcjwwa-xnfrea ')
+
+if code == 2:
+	randomint = random.randint(1,999999)
+	if randomint <= 9:
+		randomint = str(randomint)
+		randomint = '0' + randomint
+	print(str(randomint) + '-brnsam-bsjfsa-bfnrwa ')
+
+if code == 3:
+	randomint = random.randint(1,999999)
+	if randomint <= 9:
+		randomint = str(randomint)
+		randomint = '0' + randomint
+	print(str(randomint) + '-abdnwj-bcjwwa-xnfrea ')
+	
+if code == 4:
+	randomint = random.randint(1,999999)
+	if randomint <= 9:
+		randomint = str(randomint)
+		randomint = '0' + randomint
+	print(str(randomint) + '-mdbrea-btrwqa-nfneas ')
+
+print("done! have fun!")
+input()
