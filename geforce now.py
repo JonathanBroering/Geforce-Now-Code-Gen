dee710dd9a05bff9d2970be6c4325811857c91e7
Sleeyax <yourd3veloper@gmail.com>
2018-08-23 21:59:15 +0200

    Improved code efficienty

--- geforce now.py
+++ geforce now.py
@@ -1,33 +1,10 @@
 import random
 print('This is a free Project by Jonathan (Jnthn) licenced under the MIT License. [jnthn.party]')
-code = random.randint(1,4)
-if code == 1:
-	randomint = random.randint(1,999999)
-	if randomint <= 9:
-		randomint = str(randomint)
-		randomint = '0' + randomint
-	print(str(randomint) + '-abdnwj-bcjwwa-xnfrea ')
 
-if code == 2:
-	randomint = random.randint(1,999999)
-	if randomint <= 9:
-		randomint = str(randomint)
-		randomint = '0' + randomint
-	print(str(randomint) + '-brnsam-bsjfsa-bfnrwa ')
-
-if code == 3:
-	randomint = random.randint(1,999999)
-	if randomint <= 9:
-		randomint = str(randomint)
-		randomint = '0' + randomint
-	print(str(randomint) + '-abdnwj-bcjwwa-xnfrea ')
-	
-if code == 4:
-	randomint = random.randint(1,999999)
-	if randomint <= 9:
-		randomint = str(randomint)
-		randomint = '0' + randomint
-	print(str(randomint) + '-mdbrea-btrwqa-nfneas ')
+randomint = random.randint(1, 999999)
+if randomint <= 9:
+	randomint = '0' + str(randomint)
+print(str(randomint) + random.choice(['-abdnwj-bcjwwa-xnfrea', '-brnsam-bsjfsa-bfnrwa', '-mdbrea-btrwqa-nfneas']))
 
 print("done! have fun!")
 input()
